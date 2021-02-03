f148e008245ef346418d071a708bcc0dd0dc9888
Emma-Belg <emma.wishart@live.com.au>
2021-02-03 16:41:28 +0100

    mobile friendly version of header and nav

--- themes/bloemerin/header.php
+++ themes/bloemerin/header.php
@@ -32,17 +32,26 @@ $container = get_theme_mod('understrap_container_type');
 		<div class="top-header">
 			<div class="container">
 				<div class="row align-items-center">
-					<div class="col-lg-5">
-						<a href="/">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logos/Bloemerin_Logo2.png"
-								 alt="Bloemerin Logo">
-						</a>
+					<div class="col-lg-5 col-md-7">
+						<div class="mobile-holder-logo">
+							<a href="/">
+								<img src="<?php echo get_template_directory_uri(); ?>/img/logos/Bloemerin_Logo2.png"
+									 alt="Bloemerin Logo">
+							</a>
+						</div>
 					</div>
-					<div class="col-lg-7">
+					<div class="col-lg-7 col-md-5">
 						<div class="top-header-contact">
-							<a href="#"><i class="fa fa-facebook-square"></i></a>
-							<a href="#"><i class="fa fa-instagram"></i></a>
-							<a href="#"><i class="fa fa-pinterest-square"></i></a>
+							<div class="mobile-menu-dropdown-small">
+								<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'understrap'); ?>">
+									<span>Menu</span> <i class="fa fa-bars"></i>
+								</button>
+							</div>
+							<div class="top-header-social-icons">
+								<a href="#"><i class="fa fa-facebook-square"></i></a>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+								<a href="#"><i class="fa fa-pinterest-square"></i></a>
+							</div>
 						</div>
 					</div>
 				</div>
