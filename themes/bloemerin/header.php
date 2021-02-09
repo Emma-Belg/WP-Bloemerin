094ee76da6390913b723ebf4f622d5ebee665ccf
Emma-Belg <emma.wishart@live.com.au>
2021-02-09 18:55:00 +0100

    most of the content for the homepage done

--- themes/bloemerin/header.php
+++ themes/bloemerin/header.php
@@ -17,6 +17,10 @@ $container = get_theme_mod('understrap_container_type');
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
+
+<!--	Link to Moneypenny font-->
+	<link rel="stylesheet"	href="css/typeface-moneypenny.css">
+<!--	Other fonts-->
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,800;1,400&display=swap"
 		  rel="stylesheet">
 <!--	<link rel="preconnect" href="https://fonts.gstatic.com">-->
--- themes/bloemerin/page-templates/template-home.php
+++ themes/bloemerin/page-templates/template-home.php
@@ -17,6 +17,7 @@ if (is_front_page()) {
 	get_template_part('global-templates/hero');
 }
 ?>
+	<!--TODO: Change all divs to be semantic-->
 
 	<div class="home-hero-banner-top">
 		<div class="home-carousel">
@@ -46,7 +47,6 @@ if (is_front_page()) {
 							<p>filler text about Cupcakes...</p>
 							<a href="#" class="btn btn-primary">See Recipe</a>
 						</div>
-						<!--					<img class="d-block w-100" src="img/recipes/mike-meeks-zk-fclJdGas-unsplash.jpg" alt="Second slide">-->
 					</div>
 					<div class="carousel-item">
 						<img class="d-block w-100"
@@ -70,62 +70,126 @@ if (is_front_page()) {
 			</div>
 		</div>
 
-		<!--		<div class="quote-holder">-->
-		<!--			<img class="flour-img-wide" alt="life is better with flour in it"-->
-		<!--				 src="-->
-		<?php //echo get_template_directory_uri(); ?><!--/img/LifeIsBetterNoWords-Xwide.png">-->
-		<!--			<div class="bloemerin-quote">-->
-		<!--				<p class="dutch">Leven is beter met Bloemerin.</p>-->
-		<!--			</div>-->
-		<!--			<div class="bloemerin-quote">-->
-		<!--				<p class="english">Life is better with flour in it.</p>-->
-		<!--			</div>-->
-		<!--		</div>-->
-
 		<div class="quote-holder">
-
-				<div class="bloemerin-quote">
-					<p class="dutch">Leven is beter met Bloemerin.</p>
-				</div>
-				<div class="bloemerin-quote">
-					<p class="english">Life is better with flour in it.</p>
-				</div>
-
-					<img class="illustration-to-invert"
-						 src="<?php echo get_template_directory_uri(); ?>/img/flourishIllustrations/baking-ingredients-32002_1280.png"
-						 alt="baking illustration"
-					>
-		</div>
-
-		<div class="info-holder-background-underlay">
-			<div class="container-info">
-				<div class="info-holder">
-					<div class="title">
-						<i class="fa fa-globe"></i>Baking across the globe
-					</div>
-					<div class="description">
-						Hi, I am Erin. I love baking and I have been collecting inspiration from across the four
-						different continents have lived and baked in. I can't wait to share all that have I learnt
-						with you.
-						<br/>
-						<a href="#" class="btn btn-secondary">Baking Abroad</a>
-					</div>
-				</div>
+			<div class="bloemerin-quote">
+				<p class="dutch">Leven is beter met Bloemerin.</p>
 			</div>
+			<div class="bloemerin-quote">
+				<p class="english">Life is better with flour in it.</p>
+			</div>
+
+			<img class="illustration-to-invert"
+				 src="<?php echo get_template_directory_uri(); ?>/img/flourishIllustrations/baking-ingredients-32002_1280.png"
+				 alt="baking illustration"
+			>
 		</div>
 	</div>
 
 	<div class="home-product-info">
+		<div class="home-products-holder">
+			<div class="container">
+				<h2>Recent Recipes</h2>
+				<div class="home-products">
+					<div class="home-product">
+						<div class="row">
+							<div class="col-lg-5">
+								<a href="#"
+								   class="btn">
+									<img src="<?php echo get_template_directory_uri(); ?>/img/recipes/david-holifield-kPxsqUGneXQ-unsplash.jpg"
+										 alt="cake"
+										 width="100%"
+									>
+								</a>
+							</div>
+							<div class="col-lg-7">
+								<div class="title">Super Chocolate Cake</div>
+								<div class="content">This will be the first part of the blog that will be copied over in
+									here as an introduction to the recipe. More filler text will go until at some point
+									it gets cut off with an ellipsis ...
+								</div>
+								<div class="tags">
+									<div class="btn tag">Tag Name</div>
+									<div class="btn tag">Another Tag Name</div>
+									<div class="btn tag">A Third Tag</div>
+									<div class="btn tag">Another Tag Name</div>
+									<div class="btn tag">Another Tag Name</div>
+								</div>
+								<div class="btn absolute-holder-button">
+									See recipe
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="home-product">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="<?php echo get_template_directory_uri(); ?>/img/recipes/david-holifield-kPxsqUGneXQ-unsplash.jpg"
+									 alt="cake"
+									 width="100%"
+								>
+							</div>
+							<div class="col-lg-8">
+								<div class="title">Super Chocolate Cake</div>
+								<div class="content">This will be the first part of the blog that will be copied over in
+									here as an introduction to the recipe. More filler text will go until at some point
+									it gets cut off with an ellipsis ...
+								</div>
+								<div class="tags">
+									<div class="btn tag">Tag Name</div>
+									<div class="btn tag">Another Tag Name</div>
+									<div class="btn tag">A Third Tag</div>
+									<div class="btn tag">Another Tag Name</div>
+									<div class="btn tag">Another Tag Name</div>
+								</div>
+								<div class="btn absolute-holder-button">
+									See recipe
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="home-product">
+						<div class="row">
+							<div class="col-lg-4">
+								<img src="<?php echo get_template_directory_uri(); ?>/img/recipes/david-holifield-kPxsqUGneXQ-unsplash.jpg"
+									 alt="cake"
+									 width="100%"
+								>
+							</div>
+							<div class="col-lg-8">
+								<div class="title">Super Chocolate Cake</div>
+								<div class="content">This will be the first part of the blog that will be copied over in
+									here as an introduction to the recipe. More filler text will go until at some point
+									it gets cut off with an ellipsis ...
+								</div>
+								<div class="tags">
+									<div class="btn tag">Tag Name</div>
+									<div class="btn tag">Another Tag Name</div>
+									<div class="btn tag">A Third Tag</div>
+									<div class="btn tag">Another Tag Name</div>
+									<div class="btn tag">Another Tag Name</div>
+								</div>
+								<div class="btn absolute-holder-button">
+									See recipe
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
 		<div class="home-product-info-background-underlay">
 			<div class="container">
 				<div class="home-special-offer">
 					<div class="discount-circle">
-						<div class="save">
-							Want
-							<div class="highlight-word">
-								Erin's Baking in
+						<div class="absolute-holder">
+							<div class="save">
+								Want
+								<div class="highlight-word">
+									Erin's Baking in
+								</div>
+								your inbox?
 							</div>
-							your inbox?
 						</div>
 					</div>
 					<div class="entry-content">
@@ -137,6 +201,96 @@ if (is_front_page()) {
 						<a href="#" class="button"></a>
 					</div>
 				</div>
+			</div>
+		</div>
+
+		<div class="home-we-accept">
+			<div class="container">
+				<div class="title-holder">
+					<h2>Looking for Inspiration?</h2>
+					<div class="button-holder">
+						<a href="#" class="button"></a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-3 col-sm-6">
+						<div class="circle">
+							<a href="#" class="button">
+								<div class="absolute-holder">
+									<img alt="category"
+										 src="<?php echo get_template_directory_uri(); ?>/img/recipes/david-holifield-sCsjtUwN8LA-unsplash.jpg"
+										 width="100%"
+									>
+									<div class="title">
+										Category Name
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6">
+						<div class="circle">
+							<a href="#" class="button">
+								<div class="absolute-holder">
+									<img alt="category"
+										 src="<?php echo get_template_directory_uri(); ?>/img/recipes/m-w-vL6XDSdvmec-unsplash.jpg"
+										 width="100%"
+									>
+									<div class="title">
+										Category Name
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6">
+						<div class="circle">
+							<a href="#" class="button">
+								<div class="absolute-holder">
+									<img alt="category"
+										 src="<?php echo get_template_directory_uri(); ?>/img/recipes/michaela-baum-VnM6_liIRJ0-unsplash.jpg"
+										 width="100%"
+									>
+									<div class="title">
+										Category Name
+									</div>
+								</div>
+							</a>
+
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6">
+						<div class="circle">
+							<a href="#" class="button">
+								<div class="absolute-holder">
+									<img alt="category"
+										 src="<?php echo get_template_directory_uri(); ?>/img/recipes/american-heritage-chocolate-vdx5hPQhXFk-unsplash.jpg"
+										 width="100%"
+									>
+									<div class="title">
+										Category Name
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-info">
+		<div class="info-holder">
+			<div class="title">
+				<i class="fa fa-globe"></i>Baking across the globe
+			</div>
+			<div class="description">
+				Hi, I am Erin. I love baking and I have been collecting inspiration from across the four
+				different continents have lived and baked in. I can't wait to share all that have I learnt
+				with you.
+				<br/>
+				<a href="#" class="btn btn-secondary">Baking Abroad</a>
 			</div>
 		</div>
 	</div>
